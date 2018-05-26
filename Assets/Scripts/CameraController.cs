0a2883fafce130767df79b74d1492420e37097ba
David Baxter <baxter30@gmail.com>
2018-05-26 10:29:13 -0700

    created script to control camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // public declarations come first
+    public GameObject followTarget;
+    public float moveSpeed;
+
+    // then private
+    private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // keeping the update function readable and puting logic in a method.
+        // May be a good Idea to never have the logic directly in the update function. 
+        ChangeCameraPosition();
+    }
+
+    private void ChangeCameraPosition()
+    {
+        // Variable used twice so declaring it locally
+        Vector3 followTargetPosition = followTarget.transform.position;
+
+        targetPosition = new Vector3(followTargetPosition.x, followTargetPosition.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+}
